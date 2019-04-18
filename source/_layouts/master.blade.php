7a1ef8180915bb83bf88b13e76e0a3ffdad2c931
Ahmad H <ahmadawmh@gmail.com>
2019-04-18 23:15:24 +0200

    Twitter HTML meta

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -5,9 +5,13 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
         <meta property="og:title" content="أرقام الطوارئ والمساعدة">
-        <meta property="og:description" content="أرقام الطوارئ والمساعدة في ليبيا.">
+        <meta property="og:description" content="أرقام الطوارئ والمساعدة في ليبيا">
         <meta property="og:image" content="http://sos.ly/assets/images/site-thumbnail.jpg">
         <meta property="og:url" content="http://sos.ly">
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content="أرقام الطوارئ والمساعدة"/>
+        <meta name="twitter:description" content="أرقام الطوارئ والمساعدة في ليبيا" />
+        <meta name="twitter:image" content="http://sos.ly/assets/images/site-thumbnail.jpg" />
         <meta name="description" content="أرقام الطوارئ والمساعدة في ليبيا">
         <meta name="keywords" content="الدفاع المدني, فريق الطوارئ, الإسعاف, ليبيا">
         <title>أرقام الطوارئ والمساعدة</title>
