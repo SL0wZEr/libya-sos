8b3f632bafdc9e41844bc459dc4251f4bfe98a3e
Ahmad H <ahmadawmh@gmail.com>
2019-04-18 20:10:17 +0200

    Dynamic Icons

--- source/index.blade.php
+++ source/index.blade.php
@@ -4,24 +4,29 @@
     @foreach($page->contacts as $contactTitle => $contact)
         <div class="my-3 p-3 bg-white rounded shadow-sm">
             <div class="section-head border-bottom border-gray pt-2 pb-3 mb-0 d-flex align-items-center">
-                <i class="fas fa-fire ml-1 mr-2 rounded" style="fill: #007bff;"></i>
+                <i class="{{ $contact->icon }} ml-1 mr-2"></i>
                 <h6 class="font-weight-bold m-0 pt-2">{{ $contactTitle }}</h6>
             </div>
             <div class="section-body">
                 <table class="table">
                     <thead>
                         <tr>
-                            <th class="border-top-0 border-bottom-0 text-muted">البلدية</th>
-                            <th class="border-top-0 border-bottom-0 text-muted">الجهة</th>
+                            <th class="border-top-0 border-bottom-0 text-muted w-75">الجهة</th>
                             <th class="border-top-0 border-bottom-0 text-muted">رقم الهاتف</th>
                         </tr>
                     </thead>
                     <tbody>
                         @foreach($contact->branches as $branch)
                             <tr>
-                                <td>{{ $branch->district }}</td>
-                                <td>{{ $branch->name }}</td>
-                                <td><a href="tel:{{ $branch->phone_number }}">{{ $branch->phone_number }}</a></td>
+                                <td>
+                                    <p class="mb-0">{{ $branch->name }}</p>
+                                    <small class="text-muted">{{ $branch->district }}</small>
+                                </td>
+                                <td>
+                                    <a href="tel:{{ $branch->phone_number }}">
+                                        {{ $branch->phone_number }} <i class="fas fa-phone ml-1 mr-2 fa-flip-horizontal ml-2"></i>
+                                    </a>
+                                </td>
                             </tr>
                         @endforeach
                     </tbody>
