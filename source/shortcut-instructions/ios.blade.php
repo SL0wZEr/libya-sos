78c569ee3bc21eedf744809580264f0fd60c5a22
Ahmad H <ahmadawmh@gmail.com>
2019-04-18 21:34:43 +0200

    Add instruction videos

---
+++ source/shortcut-instructions/ios.blade.php
@@ -0,0 +1,10 @@
+@extends('_layouts.master')
+
+@section('body')
+    <div class="text-center">
+        <p class="my-5"><a href="/">العودة إلى الصفحة الرئيسة <i class="fas fa-arrow-left ml-2"></i></a></p>
+        <video width="384" height="848" controls>
+            <source src="/assets/images/ios.mp4" type="video/mp4">
+        </video>
+    </div>
+@endsection
