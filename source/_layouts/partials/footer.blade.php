cae04a71dc83ba19afb39a2807bb4b82d73ed899
Ahmad H <ahmadawmh@gmail.com>
2019-04-18 21:01:22 +0200

    Add page footer

---
+++ source/_layouts/partials/footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="container">
+    <div class="alert alert-warning text-center" role="alert">
+        <p class="pt-2"><strong>يمكنك حفظ هذه الصفحة كاختصار في هاتف المحمول.</strong></p>
+        <p class="mb-0">تعليمات الحفظ لأجهزة الـ <a href="/shortcut-instructions/ios">IOS <i class="fab fa-apple"></i></a> | تعليمات الحفظ لأجهزة <a href="/shortcut-instructions/android">الأندرويد <i class="fab fa-android"></i></a>.</p>
+    </div>
+
+    <p class="text-muted text-center pt-3 pb-2">
+        هذا المشروع تطوعي مفتوح المصدر ( <a href="http://github.com/sl0wZEr/libya-sos">رابط الـ GitHub</a> ) يمكنك المساهمة في تحسينه إن شئت.
+    </p>
+</footer>
